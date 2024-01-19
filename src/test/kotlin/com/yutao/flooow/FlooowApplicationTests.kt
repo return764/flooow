f7c40a657c1dc5e68fcaf6b55b4ca6cca0164025
yutao <tao.yu@thoughtworks.com>
2024-01-19 12:07:52 +0800

    Initial Commit

---
+++ src/test/kotlin/com/yutao/flooow/FlooowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yutao.flooow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlooowApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
